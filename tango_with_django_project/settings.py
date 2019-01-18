7438c40bc876614be08efd77aa64bd7bf9122dab
Scott Brown <scott_m_brown@mac.com>
2019-01-18 15:12:42 +0000

    Added media files support

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,6 +16,10 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 STATIC_DIR = os.path.join(BASE_DIR, "static")
+MEDIA_DIR = os.path.join(BASE_DIR, "media")
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = "/media/"
 
 
 # Quick-start development settings - unsuitable for production
@@ -65,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template/context_processors.media',
             ],
         },
     },
