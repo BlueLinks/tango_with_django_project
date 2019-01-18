18eee05664148958a4ac262a60551c22e0b5d719
Scott Brown <scott_m_brown@mac.com>
2019-01-18 14:04:31 +0000

    Created index page

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':
-        [TEMPLATE_DIR, ],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
