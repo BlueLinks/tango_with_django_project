a228c7427b40746db8836029de3dc0292a3d4afd
Scott Brown <scott_m_brown@mac.com>
2019-01-24 19:24:34 +0000

    Updated admin interface

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from rango.models import Category, Page
+
+admin.site.register(Category)
+admin.site.register(Page)
--- rango/models.py
+++ rango/models.py
@@ -5,7 +5,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
-    def __str__(self):  # For Python 2, use __unicode__ too
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
         return self.name
 
 
