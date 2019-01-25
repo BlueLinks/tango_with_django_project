ae317e263158760c628d69ab2927821da648309f
Scott Brown <scott_m_brown@mac.com>
2019-01-25 13:25:53 +0000

    Implemented Slug

--- rango/admin.py
+++ rango/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
 # Register your models here.
 
@@ -8,5 +9,8 @@ from rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ("title", "category", "url")
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
