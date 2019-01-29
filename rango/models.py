78abe639a7519fdf42f84d3d57da3579a6d795e9
Scott Brown <scott_m_brown@mac.com>
2019-01-29 19:05:25 +0000

    Added function to add categories and pages

--- rango/models.py
+++ rango/models.py
@@ -4,7 +4,8 @@ from django.template.defaultfilters import slugify
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    max_name_length = 128
+    name = models.CharField(max_length=max_name_length, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
@@ -21,8 +22,9 @@ class Category(models.Model):
 
 
 class Page(models.Model):
+    max_title_length=128
     category = models.ForeignKey(Category)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=max_title_length)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
