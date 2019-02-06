aaa8f90cb06d113a80c636a5a9e54c8077ef94ee
Scott Brown <scott_m_brown@mac.com>
2019-02-06 22:05:16 +0000

    Added ability to create accounts

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 # Register your models here.
 
@@ -14,3 +15,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
